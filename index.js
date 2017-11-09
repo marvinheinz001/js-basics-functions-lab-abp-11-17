9b1c098ff50f7640f2cbd4bede9be99bb4f16972
Max han <mwellh84@gmail.com>
2017-11-09 23:04:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function calculatesFarePrice(start, destination){
     return 0
   }else if(distance > 400 && distance <= 2000){
     return distance * 0.02
-  }else if(distance > 2000){
+  }else if(distance > 2000 && distance <= 2500){
     return 25
   }else if (distance > 2500){
     return 'cannot travel that far'
